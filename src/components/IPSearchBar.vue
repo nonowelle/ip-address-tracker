4540a66606e760191ccef731585200868768a21e
Nonowelle <menoel888@gmail.com>
2025-06-02 11:41:44 -0700

    style ip infos

--- src/components/IPSearchBar.vue
+++ src/components/IPSearchBar.vue
@@ -7,6 +7,24 @@
       <input type="text" placeholder="Search for any IP address or domain" />
       <button>></button>
     </div>
+    <div class="ip-info">
+      <div class="ip-info-item">
+        <h4>IP Address</h4>
+        <p class="result">Result</p>
+      </div>
+      <div class="ip-info-item">
+        <h4>Location</h4>
+        <p class="result">Result</p>
+      </div>
+      <div class="ip-info-item">
+        <h4>Timezone</h4>
+        <p class="result">Result</p>
+      </div>
+      <div class="ip-info-item">
+        <h4>ISP</h4>
+        <p class="result">Result</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,5 +85,41 @@ button {
   justify-content: center;
   font-size: 24px;
   cursor: pointer;
+}
+.ip-info {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+  font-size: 18px;
+  font-weight: 100;
+  margin-top: 2rem;
+  background-color: white;
+  border-radius: 24px;
+  padding: 30px 24px;
+  color: gray;
+  width: 90vw;
+}
+h4 {
+  font-size: 14px;
+  font-weight: 200;
+}
+.ip-info-item {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: stretch;
+  gap: 18px;
+  width: 20%;
+  border-right: 1px solid grey;
+
+  &:last-of-type {
+    border: none;
+  }
+  .result {
+    color: black;
+    font-weight: bold;
+    font-size: 24px;
+  }
 }
 </style>
